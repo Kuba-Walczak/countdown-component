d6c92498ed5833158bab97a4b8927c45cbb1bec5
Jacob <kubawalczak005@gmail.com>
2026-04-15 17:53:46 +0200

    feat: added minimal testing environment

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import Countdown from "./components/Countdown";
+
+export default function App() {
+  return (
+    <main className="relative bg-black min-h-screen flex items-center justify-center">
+      <Countdown />
+    </main>
+  );
+}
---
+++ src/components/Countdown.tsx
@@ -0,0 +1,6 @@
+export default function Countdown() {
+  return (
+    <div className="relative h-40 w-80 bg-red-500">
+    </div>
+  );
+}
