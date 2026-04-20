93870aa26b523a9af6327f1d58608a2e4fc63a93
Kuba Walczak <s33512@pjwstk.edu.pl>
2026-04-20 20:49:21 +0200

    ref: removed temporary test function

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,12 @@
-import { useMemo } from "react";
 import Countdown from "./components/Countdown";
 
 export default function App() {
-  const targetDate = useMemo(() => {
-    const date = new Date();
-    date.setSeconds(date.getSeconds() + 20);
-    return date.toISOString();
-  }, []);
-
   return (
     <main className="relative bg-background min-h-screen flex items-center justify-center p-4">
       <div className="flex flex-col items-center gap-6">
         <section className={`theme-night rounded-3xl bg-background p-4`}>
           <Countdown
-            targetDate={targetDate}
+            targetDate={"2027-04-20T15:00:00Z"}
           />
         </section>
       </div>
