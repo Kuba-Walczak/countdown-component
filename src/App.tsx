17c05da6e0fe8d5bee0b79aa9ef86cf07638738c
Jacob <kubawalczak005@gmail.com>
2026-04-20 23:26:53 +0200

    ref: cleaned up dead code

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,10 @@ import Countdown from "./components/Countdown";
 
 export default function App() {
   return (
-    <main className="relative bg-background min-h-screen flex items-center justify-center p-4">
-      <div className="flex flex-col items-center gap-6">
-        <section className={`theme-night rounded-3xl bg-background p-4`}>
-          <Countdown
-            targetDate={"2027-04-20T15:00:00Z"}
-          />
-        </section>
-      </div>
+    <main className="relative bg-background min-h-screen flex items-center justify-center">
+      <Countdown
+        targetDate={"2026-05-20T15:00:00Z"}
+      />
     </main>
   );
 }
